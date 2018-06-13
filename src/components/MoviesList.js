3a46b3804b4d14989262ec3d9f1f65bceacda43a
falinsky <sergey@falinsky.com>
2018-06-13 15:18:16 +0300

    Add presentational component for showing a list of movies

---
+++ src/components/MoviesList.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {getMoviePosterImageUrl} from '../api';
+
+const MESSAGE__LOADING = 'Loading...';
+const MESSAGE__ERROR = 'Error';
+const MESSAGE__NO_MOVIES_FOUND = 'No movies found';
+
+class MoviesList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPopularMovies();
+  }
+
+  render() {
+    const {movies, isLoading, isError} = this.props;
+
+    if (isLoading && !movies.length) {
+      return MESSAGE__LOADING;
+    }
+
+    if (isError) {
+      return MESSAGE__ERROR;
+    }
+
+    if (!movies.length) {
+      return MESSAGE__NO_MOVIES_FOUND;
+    }
+
+    return (
+      <ul>
+        {movies.map(movie => (
+          <li key={movie.id}>
+            <p>
+              {movie.title}
+            </p>
+            <img src={getMoviePosterImageUrl(movie.poster_path)} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+MoviesList.defaultProps = {
+  movies: [],
+  isLoading: false,
+  isError: false,
+};
+
+export default MoviesList;
