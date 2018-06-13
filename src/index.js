854ce3064334d066274eb2ba47f537c1c552acb2
falinsky <sergey@falinsky.com>
2018-06-13 15:40:23 +0300

    Configure store and render PopularMovies component instead of App.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+import rootReducer from './reducers';
+import PopularMovies from './containers/PopularMovies';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
+
+ReactDOM.render((
+  <Provider store={store}>
+    <PopularMovies />
+  </Provider>
+), document.getElementById('root'));
 registerServiceWorker();
