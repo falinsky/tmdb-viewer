049b533284d4a2125c680cf7bb155c64a17bbb03
falinsky <sergey@falinsky.com>
2018-06-13 16:51:47 +0300

    Export popularMovies reducer to own file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,46 +1,5 @@
 import {combineReducers} from 'redux';
-import {
-  FETCH_POPULAR_MOVIES_REQUEST,
-  FETCH_POPULAR_MOVIES_SUCCESS,
-  FETCH_POPULAR_MOVIES_FAILURE
-} from '../actions';
-
-const defaultState = {
-  results: [],
-  isFetching: false,
-  isError: false,
-};
-
-function popularMovies(state = defaultState, action) {
-  switch (action.type) {
-    case FETCH_POPULAR_MOVIES_REQUEST:
-      return {
-        ...state,
-        isFetching: true,
-        isError: false,
-      };
-
-    case FETCH_POPULAR_MOVIES_SUCCESS:
-      return {
-        isFetching: false,
-        isError: false,
-        results: [
-          ...state.results,
-          ...action.payload.results,
-        ],
-      };
-
-    case FETCH_POPULAR_MOVIES_FAILURE:
-      return {
-        ...state,
-        isFetching: false,
-        isError: true,
-      };
-
-    default:
-      return state;
-  }
-}
+import popularMovies from './popularMovies';
 
 export default combineReducers({
   popularMovies
---
+++ src/reducers/popularMovies.js
@@ -0,0 +1,42 @@
+import {
+  FETCH_POPULAR_MOVIES_REQUEST,
+  FETCH_POPULAR_MOVIES_SUCCESS,
+  FETCH_POPULAR_MOVIES_FAILURE
+} from '../actions';
+
+const defaultState = {
+  results: [],
+  isFetching: false,
+  isError: false,
+};
+
+export default function popularMovies(state = defaultState, action) {
+  switch (action.type) {
+    case FETCH_POPULAR_MOVIES_REQUEST:
+      return {
+        ...state,
+        isFetching: true,
+        isError: false,
+      };
+
+    case FETCH_POPULAR_MOVIES_SUCCESS:
+      return {
+        isFetching: false,
+        isError: false,
+        results: [
+          ...state.results,
+          ...action.payload.results,
+        ],
+      };
+
+    case FETCH_POPULAR_MOVIES_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        isError: true,
+      };
+
+    default:
+      return state;
+  }
+}
