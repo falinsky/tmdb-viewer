2f5faf1c79448335af518f83139feba918552dbb
falinsky <sergey@falinsky.com>
2018-06-13 15:19:54 +0300

    Add container component for showing a list of movies

---
+++ src/containers/PopularMovies.js
@@ -0,0 +1,15 @@
+import {connect} from 'react-redux';
+import MoviesList from '../components/MoviesList';
+import {fetchPopularMovies} from '../actions';
+
+const mapStateToProps = (state) => ({
+  movies: state.popularMovies.results,
+  isLoading: state.popularMovies.isFetching,
+  isError: state.popularMovies.isError
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchPopularMovies: () => dispatch(fetchPopularMovies()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MoviesList);
