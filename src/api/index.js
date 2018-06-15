2d377825f651c5548f158c32a8e9efd469802be6
falinsky <sergey@falinsky.com>
2018-06-15 14:25:25 +0300

    Provide ability to fetch genres list using api wrapper

--- src/api/index.js
+++ src/api/index.js
@@ -13,20 +13,38 @@ function generateQueryString(params = {}) {
   return Object.keys(params).map((key) => `${key}=${params[key]}`).join('&');
 }
 
-export function getPopularMovies(page = 1) {
-  const path = 'movie/popular';
-  const query = generateQueryString({page});
-  const url = `${BASE_API_URL}${path}?${query}`;
+function generateUrl(path, params = {}) {
+  const query = generateQueryString(params);
 
+  return `${BASE_API_URL}${path}?${query}`;
+}
+
+function throwCommonError(data) {
+  throw new Error(`${data.status_message} (error code: ${data.status_code})`);
+}
+
+function handleApiCall(url, requiredResponseParamName) {
   return fetch(url).then(response => response.json()).then(data => {
-    if (data.results) {
+    if (data[requiredResponseParamName]) {
       return data;
     }
 
-    throw new Error(`${data.status_message} (error code: ${data.status_code})`);
+    throwCommonError(data);
   });
+}
+
+export function getPopularMovies(page = 1) {
+  const url = generateUrl('movie/popular', {page});
+  
+  return handleApiCall(url, 'results');
 }
 
 export function getMoviePosterImageUrl(posterPath) {
   return `${BASE_IMG_API}w300/${posterPath}`;
+}
+
+export function getGenresListForMovies() {
+  const url = generateUrl('genre/movie/list');
+
+  return handleApiCall(url, 'genres');
 }
