11b0f38d34d7446633f363d19dfe636f3534d163
falinsky <sergey@falinsky.com>
2018-06-27 16:18:18 +0300

    Simplify entities reducer and make it more common

--- src/reducers/entities.js
+++ src/reducers/entities.js
@@ -1,8 +1,4 @@
-import {
-  FETCH_POPULAR_MOVIES_SUCCESS,
-  FETCH_MOVIE_SUCCESS,
-  FETCH_GENRES_SUCCESS,
-} from '../actions';
+import merge from 'lodash/merge';
 
 const defaultState = {
   movies: {},
@@ -10,24 +6,9 @@ const defaultState = {
 };
 
 export default function entities(state = defaultState, action) {
-  switch (action.type) {
-    case FETCH_POPULAR_MOVIES_SUCCESS:
-    case FETCH_MOVIE_SUCCESS:
-      return {
-        ...state,
-        movies: {
-          ...state.movies,
-          ...action.payload.entities.movies,
-        },
-      };
-
-    case FETCH_GENRES_SUCCESS:
-      return {
-        ...state,
-        genres: action.payload.entities.genres,
-      };
-
-    default:
-      return state;
+  if (action.payload && action.payload.entities) {
+    return merge({}, state, action.payload.entities);
   }
+
+  return state;
 }
