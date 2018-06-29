3f46e543b08e55bfdc3aa1ecec10e86ef2b5a653
falinsky <sergey@falinsky.com>
2018-06-30 02:11:00 +0300

    Convert MovieDetails component to be functional one

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -6,36 +6,28 @@ import MovieRecommendations from '../containers/MovieRecommendations';
 import FavoriteBadge from '../containers/FavoriteBadge';
 import './MovieDetails.css';
 
-class MovieDetails extends React.Component {
-  componentDidMount() {
-    this.props.fetchMovie();
-  }
-
-  render() {
-    const {movie} = this.props;
-
-    return (
-      <React.Fragment>
-        {!movie ? 'Loading movie info...' : (
-          <React.Fragment>
-            <main>
-              <h1>{movie.title}</h1>
-              <p>{movie.overview}</p>
-              <img src={getMoviePosterImageUrl(movie.backdrop_path)} alt={movie.title} />
-              <FavoriteBadge movie={movie.id} />
-            </main>
-            <section className="MovieRecommendations">
-              <h2>Recommendations</h2>
-              <MovieRecommendations movie={movie.id} />
-            </section>
-          </React.Fragment>
-        )}
-        <footer>
-          <Link to="/">Return to main page</Link>
-        </footer>
-      </React.Fragment>
-    );
-  }
+function MovieDetails({movie}) {
+  return (
+    <React.Fragment>
+      {!movie ? 'Loading movie info...' : (
+        <React.Fragment>
+          <main>
+            <h1>{movie.title}</h1>
+            <p>{movie.overview}</p>
+            <img src={getMoviePosterImageUrl(movie.backdrop_path)} alt={movie.title} />
+            <FavoriteBadge movie={movie.id} />
+          </main>
+          <section className="MovieRecommendations">
+            <h2>Recommendations</h2>
+            <MovieRecommendations movie={movie.id} />
+          </section>
+        </React.Fragment>
+      )}
+      <footer>
+        <Link to="/">Return to main page</Link>
+      </footer>
+    </React.Fragment>
+  );
 }
 
 MovieDetails.propTypes = {
@@ -45,7 +37,6 @@ MovieDetails.propTypes = {
     overview: PropTypes.string.isRequired,
     backdrop_path: PropTypes.string.isRequired,
   }),
-  fetchMovie: PropTypes.func.isRequired,
 };
 
 export default MovieDetails;
--- src/containers/MovieDetails.js
+++ src/containers/MovieDetails.js
@@ -2,16 +2,17 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import MovieDetails from '../components/MovieDetails';
 import {loadMovie} from '../actions';
+import {withDataAutoload} from '../hoc';
 
 const mapStateToProps = (state, ownProps) => ({
   movie: state.entities.movies[ownProps.match.params.id],
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  fetchMovie: () => dispatch(loadMovie(ownProps.match.params.id)),
+  loadData: () => dispatch(loadMovie(ownProps.match.params.id)),
 });
 
-const MovieDetailsContainer = connect(mapStateToProps, mapDispatchToProps)(MovieDetails);
+const MovieDetailsContainer = connect(mapStateToProps, mapDispatchToProps)(withDataAutoload(MovieDetails));
 MovieDetailsContainer.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
