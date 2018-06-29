e9546a4f4718352b6ea6e53ea45884cc44c13c83
falinsky <sergey@falinsky.com>
2018-06-30 02:16:13 +0300

    Make MovieCard container autoloadable

--- src/containers/MovieCard.js
+++ src/containers/MovieCard.js
@@ -1,12 +1,18 @@
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import MovieCard from '../components/MovieCard';
+import {loadMovie} from '../actions';
+import {withDataAutoload} from '../hoc';
 
 const mapStateToProps = (state, ownProps) => ({
   movie: state.entities.movies[ownProps.id],
 });
 
-const MovieCardContainer = connect(mapStateToProps)(MovieCard);
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  loadData: () => dispatch(loadMovie(ownProps.id)),
+});
+
+const MovieCardContainer = connect(mapStateToProps, mapDispatchToProps)(withDataAutoload(MovieCard));
 MovieCardContainer.propTypes = {
   id: PropTypes.number.isRequired,
 };
