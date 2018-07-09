24573ac0690e008604ace44ca337e774849d01ec
falinsky <sergey@falinsky.com>
2018-07-09 16:12:18 +0300

    Improve FavoriteBadge using IconButton and star icons

--- src/components/FavoriteBadge.js
+++ src/components/FavoriteBadge.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import StarIcon from '@material-ui/icons/Star';
+import {withStyles} from '@material-ui/core/styles';
 
-function FavoriteBadge({inFavorites, onAdd, onRemove}) {
+const styles = (theme) => ({
+  root: {
+    color: theme.palette.primary.dark,
+  }
+});
+
+function FavoriteBadge({classes, inFavorites, onAdd, onRemove}) {
   return (
-    <button onClick={(e) => {
+    <IconButton
+      onClick={(e) => {
         e.preventDefault();
         return inFavorites ? onRemove() : onAdd();
-    }}>
-      {`${inFavorites ? 'Remove from' : 'Add to'} Favorites`}
-    </button>
+      }}
+      title={`${inFavorites ? 'Remove from' : 'Add to'} Favorites`}
+      className={classes.root}>
+      {inFavorites ? <StarIcon/> : <StarBorderIcon/>}
+    </IconButton>
   );
-};
+}
 
 FavoriteBadge.propTypes = {
+  classes: PropTypes.object.isRequired,
   inFavorites: PropTypes.bool.isRequired,
   onAdd: PropTypes.func.isRequired,
   onRemove: PropTypes.func.isRequired,
 };
 
-export default FavoriteBadge;
+export default withStyles(styles)(FavoriteBadge);
