24dacd61f68bcbed7e32c8cc498bc97403923187
falinsky <sergey@falinsky.com>
2018-07-11 16:36:11 +0300

    Add movie's page link to SingleLineMoviesListItem's actionIcon section

--- src/components/SingleLineMoviesListItem.js
+++ src/components/SingleLineMoviesListItem.js
@@ -5,6 +5,9 @@ import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import FavoriteBadge from '../containers/FavoriteBadge';
 import {getMoviePosterImageUrl} from '../api';
+import {Link} from 'react-router-dom';
+import IconButton from '@material-ui/core/IconButton';
+import LinkIcon from '@material-ui/icons/Link';
 
 const styles = theme => ({
   title: {
@@ -13,6 +16,9 @@ const styles = theme => ({
   bar: {
     background:
       'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+  actionIcon: {
+    display: 'flex',
   },
 });
 
@@ -25,8 +31,16 @@ function SingleLineMoviesListItem({classes, movie, ...otherProps}) {
         classes={{
           root: classes.bar,
           title: classes.title,
+          actionIcon: classes.actionIcon,
         }}
-        actionIcon={<FavoriteBadge movie={movie.id} classes={{root: classes.title}}/>}
+        actionIcon={
+          <React.Fragment>
+            <FavoriteBadge movie={movie.id} classes={{root: classes.title}}/>
+            <IconButton component={Link} to={`/movie/${movie.id}`} className={classes.title}>
+              <LinkIcon/>
+            </IconButton>
+          </React.Fragment>
+        }
       />
     </GridListTile>
   );
