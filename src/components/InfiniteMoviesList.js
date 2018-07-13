6a45beab5b36a13decb9b6248fdc717ae6334dc8
falinsky <sergey@falinsky.com>
2018-07-13 15:17:02 +0300

    Implement InfiniteMoviesList component using material-ui components

--- src/components/InfiniteMoviesList.js
+++ src/components/InfiniteMoviesList.js
@@ -2,25 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import InfiniteScroll from 'react-infinite-scroller';
 import MovieCard from '../containers/MovieCard';
-import './InfiniteMoviesList.css';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import {withStyles} from '@material-ui/core/styles';
 
-function InfiniteMoviesList({movies, fetchMovies, hasMore, uniqueKey}) {
+const styles = theme => ({
+  grid: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit,
+    justifyContent: 'center',
+  },
+  gridItem: {
+    width: 400,
+  },
+  card: {
+    height: '100%',
+  }
+});
+
+function InfiniteMoviesList({classes, movies, fetchMovies, hasMore, uniqueKey}) {
   return (
-    <InfiniteScroll
-      key={uniqueKey}
-      className="InfiniteMoviesList"
-      element="ul"
-      hasMore={hasMore}
-      loadMore={fetchMovies}
-    >
-      {movies.map(id => (
-        <li className="InfiniteMoviesList-Item" key={id}>
-          <MovieCard id={id} />
-        </li>
-      ))}
+    <section>
+      <Grid
+        container
+        spacing={24}
+        key={uniqueKey}
+        className={classes.grid}
+        component={InfiniteScroll}
+        hasMore={hasMore}
+        loadMore={fetchMovies}
+      >
+        {movies.map(id => (
+          <Grid item key={id} className={classes.gridItem}>
+            <MovieCard id={id} classes={{root: classes.card}} />
+          </Grid>
+        ))}
 
-      {!movies.length && !hasMore && <li>No movies found</li>}
-    </InfiniteScroll>
+        {!movies.length && !hasMore && (
+          <Grid item>
+            <Typography>No movies found</Typography>
+          </Grid>
+        )}
+      </Grid>
+    </section>
   );
 }
 
@@ -35,6 +59,7 @@ InfiniteMoviesList.propTypes = {
   // uniqueKey param is needed to completely remount InfiniteScroll component
   // it's needed to make it possible to start new searches with page 1 passed
   uniqueKey: PropTypes.string,
+  classes: PropTypes.object.isRequired,
 };
 
-export default InfiniteMoviesList;
+export default withStyles(styles)(InfiniteMoviesList);
