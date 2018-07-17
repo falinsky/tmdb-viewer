efeda052449f7e25d29592fe14604ac70febc651
falinsky <sergey@falinsky.com>
2018-07-17 15:02:33 +0300

    Add live search

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SearchBar from 'material-ui-search-bar';
 import {withStyles} from '@material-ui/core/styles';
+import debounce from 'lodash/debounce';
 
 const styles = (theme) => ({
   root: {
@@ -17,23 +18,49 @@ const styles = (theme) => ({
   }
 });
 
-function SearchMovies({classes, query, onChange, onSubmit}) {
-  return (
-    <div className={classes.root}>
-      <SearchBar
-        value={query}
-        onChange={onChange}
-        onRequestSearch={() => {
-          if (query.trim()) {
-            onSubmit(query);
-          }
-        }}
-        classes={{
-          input: classes.input,
-        }}
-      />
-    </div>
-  );
+class SearchMovies extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleRequestSearch = this.handleRequestSearch.bind(this);
+    this.handleDebouncedRequestSearch = debounce(this.handleRequestSearch, 500);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleRequestSearch() {
+    const {query, onSubmit} = this.props;
+
+    if (query.trim()) {
+      onSubmit(query);
+    }
+  };
+
+  handleChange(value) {
+    this.props.onChange(value);
+
+    this.handleDebouncedRequestSearch(this.props.query);
+  };
+
+  componentWillUnmount() {
+    this.handleDebouncedRequestSearch.cancel();
+  }
+
+  render() {
+    const {classes, query} = this.props;
+
+    return (
+      <div className={classes.root}>
+        <SearchBar
+          value={query}
+          onChange={this.handleChange}
+          onRequestSearch={this.handleRequestSearch}
+          classes={{
+            input: classes.input,
+          }}
+        />
+      </div>
+    );
+  }
 }
 
 SearchMovies.propTypes = {
