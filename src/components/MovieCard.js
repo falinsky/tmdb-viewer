c89328811ba2c09aad899cbb1751a60eef79d30f
falinsky <sergey@falinsky.com>
2018-11-27 18:21:09 +0200

    Reorganize MovieCard component, remove genres list from it

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,30 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
 import {getMovieBackdropImageUrl, getMovieReleaseYear} from '../api';
-import Genre from '../containers/Genre';
 import FavoriteBadge from '../containers/FavoriteBadge';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import {withStyles} from '@material-ui/core/styles';
 
-const styles = theme => ({
+const styles = {
   root: {
-    display: 'block',
-    textDecoration: 'none',
+
+  },
+  actionArea: {
+    height: '100%',
   },
   media: {
     height: 0,
     paddingTop: '56.25%', // 16:9
   },
-  genre: {
-    backgroundColor: 'transparent',
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: theme.palette.primary.light,
-  }
-});
+};
 
 function MovieCard({classes, movie}) {
   if (!movie) {
@@ -33,22 +28,21 @@ function MovieCard({classes, movie}) {
   }
 
   return (
-    <Card className={classes.root} component={Link} to={`/movie/${movie.id}`}>
-      <CardMedia
-        className={classes.media}
-        image={getMovieBackdropImageUrl(movie)}
-        title={movie.title}
-      />
-      <CardHeader
-        title={movie.title}
-        subheader={getMovieReleaseYear(movie)}
-        action={
-          <FavoriteBadge movie={movie.id} />
-        }
-      />
-      <CardContent>
-        {movie.genres.map(id => <Genre id={id} key={id} classes={{root: classes.genre}} />)}
-      </CardContent>
+    <Card className={classes.root}>
+      <CardActionArea className={classes.actionArea} component={Link} to={`/movie/${movie.id}`}>
+        <CardMedia
+          className={classes.media}
+          image={getMovieBackdropImageUrl(movie)}
+          title={movie.title}
+        />
+        <CardHeader
+          title={movie.title}
+          subheader={getMovieReleaseYear(movie)}
+          action={
+            <FavoriteBadge movie={movie.id} />
+          }
+        />
+      </CardActionArea>
     </Card>
   );
 }
@@ -57,7 +51,6 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
-    genres: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
   }),
   classes: PropTypes.object.isRequired,
 };
