0100adeeb14027c3340975792740d0974489f490
falinsky <sergey@falinsky.com>
2020-08-07 23:15:34 +0300

    Update according to the latest files generated by CRA

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import {BrowserRouter as Router} from 'react-router-dom';
@@ -35,10 +35,16 @@ store.subscribe(() => {
 store.dispatch(fetchGenres());
 
 ReactDOM.render((
-  <Provider store={store}>
-    <Router>
-      <App/>
-    </Router>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router>
+        <App/>
+      </Router>
+    </Provider>
+  </React.StrictMode>
 ), document.getElementById('root'));
-registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
