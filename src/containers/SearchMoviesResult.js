858b257abe818eb125b1eabc45207e10a4a201e4
falinsky <sergey@falinsky.com>
2020-11-16 20:17:05 +0200

    Switch SearchMoviesResult component to use hooks instead of react-redux connect

--- src/containers/SearchMoviesResult.js
+++ src/containers/SearchMoviesResult.js
@@ -1,22 +1,29 @@
-import { connect } from 'react-redux';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import InfiniteMoviesList from '../components/InfiniteMoviesList';
 import { searchMovies } from '../actions';
 
-const mapStateToProps = (state) => ({
-  movieIds: state.searchMovies.items,
-  hasMore: !state.searchMovies.allFetched && state.searchMovies.query !== '',
-  isFetching: state.searchMovies.isFetching,
-  reset: state.searchMovies.reset,
-  query: state.searchMovies.query,
-});
+const SearchMoviesResult = () => {
+  const movieIds = useSelector((state) => state.searchMovies.items);
+  const hasMore = useSelector(
+    (state) => !state.searchMovies.allFetched && state.searchMovies.query !== ''
+  );
+  const isFetching = useSelector((state) => state.searchMovies.isFetching);
+  const reset = useSelector((state) => state.searchMovies.reset);
+  const query = useSelector((state) => state.searchMovies.query);
+  const dispatch = useDispatch();
 
-const mergeProps = ({ query, ...props }, { dispatch }) => ({
-  ...props,
-  fetchMovies: (page) => dispatch(searchMovies(query, page)),
-});
+  return (
+    <InfiniteMoviesList
+      movieIds={movieIds}
+      hasMore={hasMore}
+      isFetching={isFetching}
+      reset={reset}
+      fetchMovies={(page) => {
+        dispatch(searchMovies(query, page));
+      }}
+    />
+  );
+};
 
-export default connect(
-  mapStateToProps,
-  undefined,
-  mergeProps
-)(InfiniteMoviesList);
+export default SearchMoviesResult;
