d4d65d6ffd117901cb39b771bf01ff9d1143b429
falinsky <sergey@falinsky.com>
2020-11-20 04:21:32 +0200

    Rename SingleLineMoviesListItemContainer to SingleLineMoviesListDefaultItem

--- src/components/SingleLineMoviesList.js
+++ src/components/SingleLineMoviesList.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import GridList from '@material-ui/core/GridList';
-import SingleLineMoviesListItem from '../containers/SingleLineMoviesListItem';
+import SingleLineMoviesListItem from '../containers/SingleLineMoviesListDefaultItem';
 import Paper from '@material-ui/core/Paper';
 
 const styles = (theme) => ({
--- src/containers/SingleLineMoviesListDefaultItem.js
+++ src/containers/SingleLineMoviesListDefaultItem.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import SingleLineMoviesListItem from '../components/SingleLineMoviesListItem';
 
-const SingleLineMoviesListItemContainer = ({ movieId, ...otherProps }) => {
+const SingleLineMoviesListDefaultItem = ({ movieId, ...otherProps }) => {
   const movie = useSelector((state) => state.entities.movies[movieId]);
 
   return <SingleLineMoviesListItem movie={movie} {...otherProps} />;
 };
 
-SingleLineMoviesListItemContainer.propTypes = {
+SingleLineMoviesListDefaultItem.propTypes = {
   movieId: PropTypes.number.isRequired,
 };
 
-export default SingleLineMoviesListItemContainer;
+export default SingleLineMoviesListDefaultItem;
