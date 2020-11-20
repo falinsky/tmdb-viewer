1404d0c9e147e834b31b761b022b7fe8b0393d33
falinsky <sergey@falinsky.com>
2020-11-20 08:56:18 +0200

    Remove redux-saga from the project not needed anymore

--- src/store.js
+++ src/store.js
@@ -1,23 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { loadState, saveState } from './localStorage';
-import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers';
-import rootSaga from './sagas';
 
 const initialState = loadState();
-
-const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: rootReducer,
   preloadedState: initialState,
-  middleware: (getDefaultMiddleware) => [
-    ...getDefaultMiddleware(),
-    sagaMiddleware,
-  ],
 });
-
-sagaMiddleware.run(rootSaga);
 
 store.subscribe(() => {
   saveState({
