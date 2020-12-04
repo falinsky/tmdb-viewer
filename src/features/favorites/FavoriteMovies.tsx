eae62b883a92c4811cab3fac1ba1d9ca74a2cd32
falinsky <sergey@falinsky.com>
2020-12-04 14:55:57 +0200

    Convert FavoriteMovies component to tsx

--- src/features/favorites/FavoriteMovies.tsx
+++ src/features/favorites/FavoriteMovies.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import InfiniteMoviesList from '../../app/InfiniteMoviesList';
 import FavoriteMovieCard from './FavoriteMovieCard';
+import { RootState } from '../../app/store';
 
 const FavoriteMovies = () => {
-  const movieIds = useSelector((state) => state.favorites);
+  const movieIds = useSelector((state: RootState) => state.favorites);
 
   return (
     <InfiniteMoviesList
