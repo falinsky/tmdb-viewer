f36787f445882df35434530ed44573b5b3b1a0ae
falinsky <sergey@falinsky.com>
2020-12-04 14:52:14 +0200

    Convert SearchMoviesResult component to tsx

--- src/features/search-movies/SearchMoviesResult.tsx
+++ src/features/search-movies/SearchMoviesResult.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import InfiniteMoviesList from '../../app/InfiniteMoviesList';
 import { searchMovies } from './searchMoviesSlice';
+import { RootState } from '../../app/store';
 
 const SearchMoviesResult = () => {
-  const movieIds = useSelector((state) => state.searchMovies.items);
+  const movieIds = useSelector((state: RootState) => state.searchMovies.items);
   const hasMore = useSelector(
-    (state) => !state.searchMovies.allFetched && state.searchMovies.query !== ''
+    (state: RootState) =>
+      !state.searchMovies.allFetched && state.searchMovies.query !== ''
   );
-  const isFetching = useSelector((state) => state.searchMovies.isFetching);
-  const reset = useSelector((state) => state.searchMovies.reset);
+  const isFetching = useSelector(
+    (state: RootState) => state.searchMovies.isFetching
+  );
+  const reset = useSelector((state: RootState) => state.searchMovies.reset);
   const dispatch = useDispatch();
 
   return (
