2141c2a79f031a6859330884f70450b1387d3762
falinsky <sergey@falinsky.com>
2021-02-27 16:33:05 +0200

    Update LoadingIndicator to be visible when react-query queries are fetching

--- src/app/LoadingIndicator.tsx
+++ src/app/LoadingIndicator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
+import { useIsFetching } from 'react-query';
 
 const useStyles = makeStyles({
   root: {
@@ -15,8 +16,9 @@ function LoadingIndicator() {
   const isLoading = useSelector((state) =>
     Object.values(state).some((value) => value.isFetching)
   );
+  const fetchingQueries = useIsFetching();
 
-  return isLoading ? (
+  return isLoading || fetchingQueries ? (
     <LinearProgress className={classes.root} color="secondary" />
   ) : null;
 }
