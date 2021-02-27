236b572f667c832d3b8a9b00df1192ea07be0281
falinsky <sergey@falinsky.com>
2021-02-27 21:00:00 +0200

    Remove movieDetailsThunk and corresponding handling of entities state in redux store

--- src/app/entitiesSlice.ts
+++ src/app/entitiesSlice.ts
@@ -2,7 +2,6 @@ import { createSlice } from '@reduxjs/toolkit';
 import merge from 'lodash/merge';
 import { fetchPopularMovies } from '../features/popular-movies/popularMoviesSlice';
 import { searchMovies } from '../features/search-movies/searchMoviesSlice';
-import fetchMovieDetails from '../features/movie-details/movieDetailsThunk';
 import { NormalizedMovies } from './schema';
 
 type EntitiesState = NormalizedMovies;
@@ -21,11 +20,6 @@ const entitiesSlice = createSlice({
     });
 
     builder.addCase(searchMovies.fulfilled, (state, action) => {
-      state.movies = merge(state.movies, action.payload.entities.movies);
-    });
-
-    builder.addCase(fetchMovieDetails.fulfilled, (state, action) => {
-      // FIXME: items returned by movie details contain more data than returned in movies lists - so check if it needs to use a separate state to store such movie details.
       state.movies = merge(state.movies, action.payload.entities.movies);
     });
   },
