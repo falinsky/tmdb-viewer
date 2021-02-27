f9879da611922167b444df65306600ed37e8ac41
falinsky <sergey@falinsky.com>
2021-02-27 15:34:38 +0200

    Retrieve movie data for MovieDetails component using react-query

--- src/features/movie-details/MovieDetails.tsx
+++ src/features/movie-details/MovieDetails.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import {
+  getMovie,
   getMoviePosterImageUrl,
   getMovieReleaseYear,
 } from '../../tmdb-api/api';
@@ -13,11 +14,9 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Rating from './Rating';
-import { useDispatch, useSelector } from 'react-redux';
-import fetchMovieDetails from './movieDetailsThunk';
-import { RootState } from '../../app/store';
 import { MovieID } from '../../tmdb-api/types';
 import Chip from '@material-ui/core/Chip';
+import { useQuery } from 'react-query';
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -57,75 +56,66 @@ function MovieDetails({ match }: MovieDetailsProps) {
   const { movieId: id } = match.params;
   const movieId = Number(id) as MovieID;
   const classes = useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchMovieDetails(movieId));
-  }, [movieId, dispatch]);
-
-  const movie = useSelector(
-    (state: RootState) => state.entities.movies[movieId]
-  );
-
-  // FIXME: temporary retrieve genres
-  const genres = useSelector((state: RootState) =>
-    movie?.genres.map((genreId) => state.entities.genres[genreId])
+  const { isLoading, data: movie } = useQuery(['movieDetails', movieId], () =>
+    getMovie(movieId)
   );
 
   return (
     <section>
-      {!movie ? (
+      {isLoading ? (
         <Typography>Loading movie info...</Typography>
       ) : (
-        <React.Fragment>
-          <Card className={classes.card} component="article">
-            <CardMedia
-              className={classes.media}
-              image={getMoviePosterImageUrl(movie)}
-              title={movie.title}
-            />
-            <div className={classes.contentWrapper}>
-              <CardHeader
-                title={
-                  <Typography variant="h5" gutterBottom>
-                    {movie.title}{' '}
-                    <time dateTime={movie.release_date}>
-                      ({getMovieReleaseYear(movie)})
-                    </time>
-                  </Typography>
-                }
-                action={<FavoriteBadge movieId={movie.id} />}
-                avatar={<Rating value={movie.vote_average * 10} />}
-                subheader={`User score based on ${movie.vote_count} ${
-                  movie.vote_count > 1 ? 'votes' : 'vote'
-                }`}
+        movie && (
+          <React.Fragment>
+            <Card className={classes.card} component="article">
+              <CardMedia
+                className={classes.media}
+                image={getMoviePosterImageUrl(movie)}
+                title={movie.title}
               />
-              <CardContent>
-                <section className={classes.section}>
-                  <Typography variant="h6" gutterBottom>
-                    Overview
-                  </Typography>
-                  <Typography gutterBottom>{movie.overview}</Typography>
-                </section>
-                <section className={classes.section}>
-                  <Typography variant="h6" gutterBottom>
-                    Genres
-                  </Typography>
-                  <div className={classes.genresList}>
-                    {genres.map((genre) => (
-                      <Chip
-                        className={classes.genreItem}
-                        label={genre.name}
-                        key={genre.id}
-                      />
-                    ))}
-                  </div>
-                </section>
-              </CardContent>
-            </div>
-          </Card>
-          <MovieRecommendations movieId={movie.id} title="Recommendations" />
-        </React.Fragment>
+              <div className={classes.contentWrapper}>
+                <CardHeader
+                  title={
+                    <Typography variant="h5" gutterBottom>
+                      {movie.title}{' '}
+                      <time dateTime={movie.release_date}>
+                        ({getMovieReleaseYear(movie)})
+                      </time>
+                    </Typography>
+                  }
+                  action={<FavoriteBadge movieId={movie.id} />}
+                  avatar={<Rating value={movie.vote_average * 10} />}
+                  subheader={`User score based on ${movie.vote_count} ${
+                    movie.vote_count > 1 ? 'votes' : 'vote'
+                  }`}
+                />
+                <CardContent>
+                  <section className={classes.section}>
+                    <Typography variant="h6" gutterBottom>
+                      Overview
+                    </Typography>
+                    <Typography gutterBottom>{movie.overview}</Typography>
+                  </section>
+                  <section className={classes.section}>
+                    <Typography variant="h6" gutterBottom>
+                      Genres
+                    </Typography>
+                    <div className={classes.genresList}>
+                      {movie.genres.map((genre) => (
+                        <Chip
+                          className={classes.genreItem}
+                          label={genre.name}
+                          key={genre.id}
+                        />
+                      ))}
+                    </div>
+                  </section>
+                </CardContent>
+              </div>
+            </Card>
+            <MovieRecommendations movieId={movie.id} title="Recommendations" />
+          </React.Fragment>
+        )
       )}
     </section>
   );
